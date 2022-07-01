150c3b12924dab13913390a8a73a30dc638e9516
Chris <nebochristian9@gmail.com>
2022-06-30 17:13:31 -0700

    My program had no return statement, so i had to fix that

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,4 +16,5 @@ printf("Size of an int: %lu byte(s)\n", sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", sizeof(l));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
 printf("Size of a float: %lu byte(s)\n", sizeof(d));
+return (0);
 }
