7db07a7c04d842ef2ffac381eda14363400e5d9b
Chris <nebochristian9@gmail.com>
2022-07-10 10:14:05 -0700

    My first commit including my README file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: This program is used to print "_putchar" to screen
+ * Return: 0 success, otherwise failure
+ */
+
+int main()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# This directory will contain codes on Function and Nested for loops
