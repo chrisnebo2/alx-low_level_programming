bbb87c7108772097b977a5bb385d4458b5cc4196
Chris <nebochristian9@gmail.com>
2022-07-13 10:37:18 -0700

    This function only prints odd numbers in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *  puts2 - prints a string, followed by a new line,
+ *   @str: pointer to the string to print
+ *    Return: void
+ */
+
+void puts2(char *str)
+{
+	int len;
+	int i;
+
+	for (len = 0; str[len]; len++)
+		;
+
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 == 1)
+			continue;
+
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
