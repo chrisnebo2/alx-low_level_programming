7916a6e217416df7a5fdbdfc1f9b0aee2886997c
Chris <nebochristian9@gmail.com>
2022-07-19 11:32:41 -0700

    This program copies character to memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+
+/**
+ * _memset -> This memory set function 
+ * @s: String
+ * @b: a character
+ * @n: an integer
+ * Return: a string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		s[a] = b;
+
+	return (s);
+}
