e7279561c0f2e4d1d62e702a691e7e447c5c335b
Chris <nebochristian9@gmail.com>
2022-07-26 04:58:31 -0700

    Just created my first C static library

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * main - This is the main entry point, used mainly to test my static library
+ * Return: (0) Success otherwise failure
+ */
+
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
